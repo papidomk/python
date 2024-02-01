a81ae2356597f3e1bfa153e82445cc90be886b5a
papido <abdelhafidepapido@gmail.com>
2024-02-01 23:26:52 +0100

    Programme Calculator

--- exBoucle.py
+++ exBoucle.py
@@ -202,26 +202,28 @@
 # if num == 0 and valeur != n :
 #     print("vous avez pas trouver dommage")
 
-# while quitter_continuer :
-#     ope = input("Entrer l'operation que vous voulez : ")
-#     while ope != "%" or ope != "+" or ope != "-" or ope != "/" or ope != "*" or ope != "**":
-#         ope = input("Entrer l'operation que vous voulez :")
-#     n1 = int(input("Entrer le premier nombre :"))
-#     n2 = int(input("Entrer le deuxieme nombre :"))
-#     if ope == "+" :
-#         print("la somme des 2 est :",n1 + n2)
-#     elif ope == "/" and n1 != 0 and n2 != 0 :
-#         print("la division est :",n1/n2)
-#     elif ope == "*" :
-#         print("le resultat de la miltiplication est :",n1*n2)
-#     elif ope == "-" :
-#         print("n1 - n2 =",n1 - n2)
-#     elif ope == "**" :
-#         print("n1^n2 =",n1 ** n2)
-#     else :
-#         print("le reste de n1 / n2 :",n1 % n2)
-#     quitter_continuer = input("continuer ou arreter : ")
-#     if quitter_continuer == "continuer" or quitter_continuer == "Continuer":
-#         continue
-#     else : 
-#         break
+ope = str(input("Entrer l'operation que vous voulez : "))
+while ope != "%" and ope != "+" and ope != "-" and ope != "/" and ope != "*" and ope != "**":
+    print("Operation non valide !")
+    ope = input("Entrer une operation entre / , + , - , % , * , ** : ")
+    while ope == "%" or ope == "+" or ope == "-" or ope == "/" or ope == "*" or ope == "**":
+        n1 = int(input("Entrer le premier nombre : "))
+        n2 = int(input("Entrer le deuxieme nombre : "))
+        if ope == "+" :
+            print("la somme des 2 est :",n1 + n2)
+        elif ope == "/" and n2 != 0 :
+            print("la division est :",n1/n2)
+        elif ope == "*" :
+            print("le resultat de la miltiplication est :",n1*n2)
+        elif ope == "-" :
+            print("n1 - n2 =",n1 - n2)
+        elif ope == "**" :
+            print("n1^n2 =",n1 ** n2)
+        else :
+            print("le reste de n1 / n2 :",n1 % n2)
+        quitter_continuer = input("continuer ou arreter : ")
+        if quitter_continuer == "continuer" or quitter_continuer == "Continuer":
+            ope = str(input("Entrer l'operation que vous voulez : "))
+            continue
+        else : 
+            break
